8d7e649fae8b9f85c4180d87bc1dd2dc102960ea
Judee554 <dinnishajohn@gmail.com>
2026-04-14 12:00:50 -0400

    Final Django deployment setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0-yzla&37t4+@_0v1yv#r8d40lb^7yl&1@u@y1@=w-nfmpry+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/var/www/django-polls-app/static/'
